3edcca24b56bf8cf36f20252036e41e61e4d3e84
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2022-03-01 16:16:17 +0530

    List article by tag fix (change to normal function from arrow function and use 'this'

--- src/components/VTag.vue
+++ src/components/VTag.vue
@@ -16,7 +16,9 @@ export default {
     }
   },
   computed: {
-    homeRoute: () => ({ name: "home-tag", params: { tag: name } })
+    homeRoute: function() {
+      return { name: "home-tag", params: { tag: this.name } };
+    }
   }
 };
 </script>
